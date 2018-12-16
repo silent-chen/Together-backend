9ad88c1edb9a0aff8aa4a2c3f9891d23eacade50
silent-chen <mc4572@columbia.edu>
2018-12-15 19:29:23 -0500

    github checkuser

--- routes/oauth.js
+++ routes/oauth.js
@@ -2,6 +2,9 @@
 const logging = require('../utils/logging');
 const moduleName = 'OAuth';
 const axios = require('axios');
+const bo = require("../resources/bo/customers");
+const return_codes = require('../utils/return_codes');
+const security = require("../utils/security");
 
 let environment_name = process.env.eb_environment;
 if(!environment_name) {
@@ -13,6 +16,7 @@ const env = require('../env').getEnv(environment_name);
 
 let post = function(req, response, next) {
 
+    let context = {tenant: req.tenant};
     let functionName = "post:";
 
     const data = req.body;
@@ -38,7 +42,15 @@ let post = function(req, response, next) {
                 (res)=>{
                     console.log("User data: ", res.data);
                     response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({ authorized: true, data: { email: res.data.email, username: res.data.login } }));
+                    let data = { email: res.data.email, username: res.data.login };
+                    checkCustomer(data,site,context).then((result) =>{
+                            response.status(200).json(result);
+                        },
+                        (err) =>{
+                            response.status(400).json(err)
+                        }
+                    );
+//                    response.send(JSON.stringify({ authorized: true, data: { email: res.data.email, username: res.data.login } }));
                 }
             )
         }, (err)=>{
@@ -46,6 +58,42 @@ let post = function(req, response, next) {
             response.send(JSON.stringify({ authorized: false }));
         });
     }
-}
-
+};
+let checkCustomer = function(data,site,context){
+    return new Promise((resolve,reject) => {
+        console.log(data);
+        let email = data.email;
+        let method = site;
+        let username = data.username;
+        bo.retrieveByTemplate({email,method}).then(
+            (result) => {
+                if (result.length === 0){
+                    return bo.retrieveByTemplate({username});
+                }
+                else {
+                    let claim = security.generate_customer_claims(data, context);
+                    let user = return_codes.codes.login_success;
+                    user.token = claim;
+                    user.username = username;
+                    resolve(user);
+                }
+            }
+        ).then( (result) => {
+            if(!Array.isArray(result))
+                return;
+            if(result.length === 0) {
+                data.pw = '123';
+                data.method = site;
+                bo.create(data,context).then((result) => {
+                    resolve(result)},
+                    (err) => {reject(err)} );
+                }
+            else {
+                let user = {username, email};
+                reject(user);
+            }
+        }
+        )
+    })
+};
 exports.post = post;
